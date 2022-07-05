20e14e5307b696b6f77dc4a470acda9caca5bd1d
iamdavidzeng <iamdavidzeng@163.com>
2022-07-05 16:42:07 +0800

    Update apollo configuration

--- src/public-gateway/apollo-configuration.ts
+++ src/public-gateway/apollo-configuration.ts
@@ -1,12 +1,25 @@
 import { ApolloServer } from "apollo-server-express";
 import { generateTypeGraphqlSchema } from "./type-graphql";
 
+const getApolloConfig = () => {
+  if (process.env.APOLLO_ENGINE_API_KEY) {
+    return {
+      key: process.env.APOLLO_ENGINE_API_KEY,
+      graphRef: `${process.env.APOLLO_ENGINE_API_KEY.split(':')[1]}@${(
+        process.env.ENV || 'stage'
+      ).toLowerCase()}`,
+    };
+  }
+  return undefined;
+};
+
 export async function generateApolloConfiguration() {
   const schema = await generateTypeGraphqlSchema();
   return {
     schema,
     introspection: true,
     debug: process.env.APOLLO_DEBUG === "true",
+    apollo: getApolloConfig(),
     plugins: [],
     context: ({ req, res }) => ({
       req,
