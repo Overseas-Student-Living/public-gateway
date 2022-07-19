fb0f846cd5317417eeebfa3170779939c1cefff9
iamdavidzeng <iamdavidzeng@163.com>
2022-07-19 14:56:48 +0800

    Remove duplicated directory

--- src/open-gateway/middlewares/call-id.ts
+++
@@ -1,8 +0,0 @@
-import * as uuid from "uuid/v4";
-
-export function callIdMiddleware({ serviceName }) {
-  return (req, _, next) => {
-    req.callId = `${serviceName}.${req.path}.${uuid()}`;
-    next();
-  };
-}
